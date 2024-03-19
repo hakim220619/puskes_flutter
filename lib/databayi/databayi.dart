83c8785d41110bb49ee3e9f80f72be152a2a2bfb
Dani lukman <danilukman2206@gmail.com>
2024-03-20 00:29:30 +0700

    asd

--- lib/databayi/databayi.dart
+++ lib/databayi/databayi.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:puskes/databayi/editdata.dart';
 import 'package:puskes/home/HomePage.dart';
 import 'package:puskes/keluhan/verifikasiadmin.dart';
@@ -11,6 +12,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DatabAyiPage extends StatefulWidget {
   const DatabAyiPage({
@@ -60,6 +62,31 @@ class _DatabAyiPageState extends State<DatabAyiPage> {
     });
   }
 
+  _launchURL(url) async {
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> cetakPdf() async {
+    // print('PDf');
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    var token = preferences.getString('token');
+    var url = Uri.parse('${dotenv.env['url']}/exportPdf');
+    final response = await http.get(url, headers: {
+      "Accept": "application/json",
+      "Authorization": "Bearer $token",
+    });
+    // print(response.body);
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      _launchURL(data['file']);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,18 +96,16 @@ class _DatabAyiPageState extends State<DatabAyiPage> {
           style: TextStyle(color: Colors.white),
         ),
         centerTitle: true,
-            leading: InkWell(
-              onTap: () {
-                Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const Homepage()));
-              },
-              child: const Icon(
-                Icons.arrow_back_ios,
-                color: Color.fromARGB(253, 255, 252, 252),
-              ),
-            ),
+        leading: InkWell(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const Homepage()));
+          },
+          child: const Icon(
+            Icons.arrow_back_ios,
+            color: Color.fromARGB(253, 255, 252, 252),
+          ),
+        ),
         backgroundColor: Colors.blue[300],
       ),
       body: RefreshIndicator(
@@ -92,47 +117,51 @@ class _DatabAyiPageState extends State<DatabAyiPage> {
             child: Column(
               children: [
                 ListTile(
-                      title: Text(
-                        "Nama: ${_listsData[index]['name']}",
-                        style: const TextStyle(
-                            fontSize: 15.0, fontWeight: FontWeight.bold),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
+                  title: Text(
+                    "Nama: ${_listsData[index]['name']}",
+                    style: const TextStyle(
+                        fontSize: 15.0, fontWeight: FontWeight.bold),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  subtitle: Text(
+                    "Email: ${_listsData[index]['email']} \nNik: ${_listsData[index]['nik']}",
+                    maxLines: 2,
+                    style: const TextStyle(fontSize: 14.0),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditDataBayi(
+                          id: _listsData[index]['id'].toString(),
+                          nik: _listsData[index]['nik'].toString(),
+                          nama: _listsData[index]['name'].toString(),
+                          email: _listsData[index]['email'].toString(),
+                          address: _listsData[index]['address'].toString(),
+                          jenisKelamin:
+                              _listsData[index]['jenis_kelamin'].toString(),
+                          tanggalLahir:
+                              _listsData[index]['tanggal_lahir'].toString(),
+                          bbLahir: _listsData[index]['bb_lahir'].toString(),
+                          tbLahir: _listsData[index]['tb_lahir'].toString(),
+                          namaOrtu: _listsData[index]['nama_ortu'].toString(),
+                        ),
                       ),
-                      subtitle: Text(
-                        "Email: ${_listsData[index]['email']} \nNik: ${_listsData[index]['nik']}",
-                        maxLines: 2,
-                        style: const TextStyle(fontSize: 14.0),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      onTap: () {
-                        
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => EditDataBayi(
-                                id: _listsData[index]['id'].toString(),
-                                nik: _listsData[index]['nik'].toString(),
-                                nama: _listsData[index]['name'].toString(),
-                                email: _listsData[index]['email'].toString(),
-                                address: _listsData[index]['address'].toString(),
-                                jenisKelamin: _listsData[index]['jenis_kelamin'].toString(),
-                                tanggalLahir: _listsData[index]['tanggal_lahir'].toString(),
-                                bbLahir: _listsData[index]['bb_lahir'].toString(),
-                                tbLahir: _listsData[index]['tb_lahir'].toString(),
-                                namaOrtu: _listsData[index]['nama_ortu'].toString(),
-                                  ),
-                          ),
-                        );
-                      },
-                    
-                  
+                    );
+                  },
                 ),
               ],
             ),
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: cetakPdf,
+        tooltip: 'Increment',
+        child: const Icon(Icons.picture_as_pdf),
+      ), // Th
     );
   }
 }
